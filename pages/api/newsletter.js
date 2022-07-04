63c5f7a74c327018b01fadc566e0fdf8c0af3f2e
fs-projects <fs.projectsuk@gmail.com>
2022-07-04 19:18:54 +0530

    Add: mongodb package and connect newsletter submission to database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,24 @@
-function handler(req, res) {
-  if (req.method === 'POST') {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
     const userEmail = req.body.email;
 
-    if (!userEmail || !userEmail.includes('@')) {
-      res.status(422).json({ message: 'Invalid email address.' });
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address." });
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({ message: 'Signed up!' });
+    const client = await MongoClient.connect(
+      "mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
+    res.status(201).json({ message: "Signed up!" });
   }
 }
 
